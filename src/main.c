4cc6d3f20a1a6ba6172e8daf78287a26bc7c5917
Piotr Kulas <pkulas@sigma.ug.edu.pl>
2011-03-03 15:59:17 +0100

    Już mamy paski, byle zmienić kolor :D

--- src/main.c
+++ src/main.c
@@ -1,16 +1,40 @@
 #include <allegro.h>
-int main() {
+//kompilacja gcc -o test main.c `allegro-config --cflags --libs`
+
+int main()
+{
     allegro_init();
     install_keyboard();
     set_color_depth(24);
-    set_gfx_mode(GFX_AUTODETECT_WINDOWED,640,480,0,0);
+    set_gfx_mode(GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0);
     set_palette(default_palette);
-	//Kod powyżej będziemy używać zawsze do naszego projektu
-    clear_to_color(screen, makecol(0,255,0)); //ustawianie koloru tła
- 
-    readkey(); // Program oczekuje na dowolny klawisz
- 
+    //Kod powyżej będziemy używać zawsze do naszego projektu
+    clear_to_color(screen, makecol(0, 255, 0));	//ustawianie koloru tła
+	BITMAP *bufor = NULL;
+    bufor = create_bitmap(640, 480);	// stosujemy podwójne buforowanie by nie migał ekran
+    if (!bufor) {
+	set_gfx_mode(GFX_TEXT, 0, 0, 0, 0);
+	allegro_message("Nie mogę utworzyć bufora !");
+	allegro_exit();
+	return 0;
+    }
+    while (!key[KEY_ESC]) {   //pętla dopóki nikt nie wciśnie ESC
+	clear_to_color(bufor, makecol(150,150,150));
+
+	//rysujemy górny pasek i go wypełniamy kolorem
+	rect(bufor,0,0, 640, 50, makecol(30,30,30));
+	floodfill(bufor,10,10,makecol(20,120,20));
+
+	//rysujemy dolny pasek i go wypełniamy kolorem
+	rect(bufor,0,450, 640, 480, makecol(30,30,30));
+	floodfill(bufor,635,470,makecol(20,120,20));
+
+	blit( bufor, screen, 0,0,0,0, 640,480); //kopiujemy bufor na ekran
+    }
+    readkey();			// Program oczekuje na dowolny klawisz
+
     allegro_exit();
     return 0;
 }
+
 END_OF_MAIN();
